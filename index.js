9a5219c1f018090a5994db0c3ddbe458bdaeef0c
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-21 22:06:25 +0600

    Fixed submit quiz button bugs

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
